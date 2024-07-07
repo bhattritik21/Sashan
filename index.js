3fa1aacdd1b68e2869296e037e8b4a8ced08cd1f
ritik_cris <62047462+bhattritik21@users.noreply.github.com>
2024-07-07 19:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow requests from this origin (your frontend during development)
+  origin: '*', // Allow requests from this origin (your frontend during development)
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
 };
