0c7eada4f1106667fd33e816575dd63b75531e67
bhattritik21 <bhattritik21@gmail.com>
2024-07-07 10:28:45 +0530

    Added code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(cors());
+app.use(cors({ origin: 'https://sashan.vercel.app', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type'], }));   
 app.use(express.json());
 
 app.post('/api/image', async (req, res) => {
